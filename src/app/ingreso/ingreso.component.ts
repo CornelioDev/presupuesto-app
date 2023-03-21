4270b4d75780eb99812754e6e91f6e82bc409735
José Manuel Cornelio <batcornelio@gmail.com>
2023-03-21 17:45:45 -0400

    Listado de elementos del componente Ingreso

--- src/app/ingreso/ingreso.component.ts
+++ src/app/ingreso/ingreso.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Egreso } from '../egreso/egreso.model';
+import { EgresoService } from '../egreso/egreso.service';
+import { Ingreso } from './ingreso.model';
+import { IngresoService } from './ingreso.service';
 
 @Component({
   selector: 'app-ingreso',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingreso.component.css']
 })
 export class IngresoComponent implements OnInit {
-
-  constructor() { }
+  
+  ingresos:Ingreso[] = [];
+  egresos: Egreso[] = [];
+  constructor(private ingresosServicio:IngresoService, private egresoServicio:EgresoService) { }
 
   ngOnInit(): void {
+    this.ingresos = this.ingresosServicio.ingresos;
+    this.egresos = this.egresoServicio.egresos;
   }
 
 }
